ce306fabf17ac28fdb30ed02e81e53dc3676c093
Vianney Bajart <vianney@redmintnetwork.fr>
2018-01-08 10:51:16 +0100

    misc: add more time conversion helpers + unit tests

--- python/loudml_new/__init__.py
+++ python/loudml_new/__init__.py
@@ -3,7 +3,7 @@ Loud ML
 """
 
 import datetime
-import dateutil
+import dateutil.parser
 
 def ts_to_datetime(ts):
     """
@@ -11,15 +11,43 @@ def ts_to_datetime(ts):
     """
     return datetime.datetime.fromtimestamp(ts, datetime.timezone.utc)
 
-def make_datetime(val):
+def ts_to_str(ts):
     """
-    Build a datetime object
+    Convert timestamp to string
+    """
+    return datetime_to_str(ts_to_datetime(ts))
+
+def str_to_datetime(string):
+    """
+    Convert string to timestamp
+    """
+    return dateutil.parser.parse(string)
+
+def str_to_ts(string):
+    """
+    Convert string to timestamp
+    """
+    return str_to_datetime(string).timestamp()
+
+def make_datetime(mixed):
+    """
+    Build a datetime object from a mixed input (second timestamp or string)
     """
 
     try:
-        return ts_to_datetime(int(val))
+        return ts_to_datetime(float(mixed))
     except ValueError:
-        return dateutil.parser.parse(val)
+        return str_to_datetime(mixed)
+
+def make_ts(mixed):
+    """
+    Build a timestamp from a mixed input (second timestamp or string)
+    """
+
+    try:
+        return float(mixed)
+    except ValueError:
+        return str_to_ts(mixed)
 
 def datetime_to_str(dt):
     """
---
+++ python/tests/test_misc.py
@@ -0,0 +1,46 @@
+import datetime
+import unittest
+
+from loudml_new import (
+    make_datetime,
+    make_ts,
+    str_to_ts,
+    ts_to_str,
+)
+
+class TestMisc(unittest.TestCase):
+    def test_datetime(self):
+        expected = datetime.datetime(
+            year=2018,
+            month=1,
+            day=8,
+            hour=9,
+            minute=39,
+            second=26,
+            microsecond=123000,
+            tzinfo=datetime.timezone.utc,
+        )
+        self.assertEqual(
+            make_datetime(1515404366.123),
+            expected,
+        )
+        self.assertEqual(
+            make_datetime("2018-01-08T09:39:26.123Z"),
+            expected,
+        )
+        self.assertEqual(
+            make_ts(1515404366.123),
+            1515404366.123,
+        )
+        self.assertEqual(
+            make_ts("2018-01-08T09:39:26.123Z"),
+            1515404366.123,
+        )
+        self.assertEqual(
+            ts_to_str(1515404366.123),
+            "2018-01-08T09:39:26.123Z",
+        )
+        self.assertEqual(
+            str_to_ts("2018-01-08T09:39:26.123Z"),
+            1515404366.123,
+        )
