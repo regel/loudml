f65a51dd4a6eb82d09deddff1870518eeea69f7f
Vianney Bajart <vianney@redmintnetwork.fr>
2018-01-11 18:38:09 +0100

    loudml: have a local file storage implementation
    Storing models in InfluxDB is not possible because it is time-sieries
    oriented. Use local filesystem instead.
    
    Signed-off-by: Vianney Bajart <vianney@redmintnetwork.fr>

---
+++ python/loudml_new/filestorage.py
@@ -0,0 +1,77 @@
+"""
+LoudML file storage
+"""
+
+import copy
+import glob
+import os
+import json
+
+from . import (
+    errors,
+)
+
+from .storage import (
+    Storage,
+)
+
+class FileStorage(Storage):
+    """
+    File storage
+    """
+
+    def __init__(self, path):
+        self.path = path
+        self.model_dir = os.path.join(path, 'models')
+
+        os.makedirs(self.model_dir, exist_ok=True)
+
+    def model_path(self, name):
+        """
+        Build model path
+        """
+        return os.path.join(self.model_dir, "%s.lmm" % name)
+
+    def _write_model(self, path, data):
+        with open(path, 'w') as model_file:
+            data = copy.deepcopy(data)
+            try:
+                del data['name']
+            except KeyError:
+                pass
+            json.dump(data, model_file)
+
+    def create_model(self, model):
+        model_path = self.model_path(model.name)
+
+        if os.path.exists(model_path):
+            raise errors.ModelExists()
+
+        self._write_model(model_path, model.data)
+
+    def save_model(self, model):
+        self._write_model(self.model_path(model.name), model.data)
+
+    def delete_model(self, name):
+        os.unlink(self.model_path(name))
+
+    def get_model(self, name):
+        model_path = self.model_path(name)
+        try:
+            with open(model_path) as model_file:
+                data = json.load(model_file)
+                data['settings']['name'] = name
+                return data
+        except FileNotFoundError:
+            raise errors.ModelNotFound()
+
+    def set_threshold(self, name, threshold):
+        data = self.get_model(name)
+        data['settings']['threshold'] = threshold
+        self._write_model(os.path.join(name), data)
+
+    def list_models(self):
+        return [
+             os.path.splitext(os.path.basename(path))[0]
+             for path in glob.glob(self.model_path('*'))
+        ]
---
+++ python/tests/test_filestorage.py
@@ -0,0 +1,70 @@
+import datetime
+import logging
+import tempfile
+import unittest
+
+logging.getLogger('tensorflow').disabled = True
+
+from loudml_new import (
+    errors,
+)
+
+from loudml_new.times import TimesModel
+from loudml_new.filestorage import FileStorage
+
+FEATURES = [
+    {
+        'name': 'avg_foo',
+        'metric': 'avg',
+        'field': 'foo',
+        'nan_is_zero': True,
+    },
+]
+
+class TestFileStorage(unittest.TestCase):
+    def test_create_and_list(self):
+        with tempfile.TemporaryDirectory() as tmp:
+            storage = FileStorage(tmp)
+
+            # Create
+            model = TimesModel(dict(
+                name='test-1',
+                index='test',
+                offset=30,
+                span=300,
+                bucket_interval=3,
+                interval=60,
+                features=FEATURES,
+                threshold=30,
+            ))
+            storage.create_model(model)
+
+            # Create
+            model = TimesModel(dict(
+                name='test-2',
+                index='test',
+                offset=56,
+                span=200,
+                bucket_interval=20,
+                interval=120,
+                features=FEATURES,
+                threshold=30,
+            ))
+            storage.create_model(model)
+
+            # List
+            self.assertEqual(storage.list_models(), ["test-1", "test-2"])
+
+            # Delete
+            storage.delete_model("test-1")
+            self.assertEqual(storage.list_models(), ["test-2"])
+
+            with self.assertRaises(errors.ModelNotFound):
+                storage.get_model("test-1")
+
+            # Rebuild
+            data = storage.get_model("test-2")
+            model = TimesModel(data['settings'], data.get('state'))
+            self.assertEqual(model.name, 'test-2')
+            self.assertEqual(model.index, 'test')
+            self.assertEqual(model.offset, 56)
