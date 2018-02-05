792308d0afa6a12249043c91653238e0ddffe5de
Vianney Bajart <vianney@redmintnetwork.fr>
2018-02-05 16:23:19 +0100

    schemas: remove test on key length
    Constraint on key length has been removed in an earlier commit.

--- loudml/tests/test_schemas.py
+++ loudml/tests/test_schemas.py
@@ -21,10 +21,8 @@ class TestSchemas(unittest.TestCase):
         self.valid("Foo-Bar")
         self.valid("00_foo_00_bar_001")
         self.valid("_foo")
-        self.valid("12345678901234567890123456789012")
 
         self.invalid("foo@bar")
         self.invalid("")
         self.invalid("foo/bar")
         self.invalid(".foo")
-        self.invalid("123456789012345678901234567890123")
