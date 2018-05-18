553389f344772c7441461f0a9339023bd8e2736d
Christophe Osuna <christophe@redmintnetwork.fr>
2018-05-18 14:37:41 +0900

    vendor: add vendor mechanism for Debian

--- loudml/loudml/vendor.py
+++ loudml/loudml/vendor.py
@@ -4,5 +4,11 @@ LoudML vendor environment
 
 import os
 import sys
+
+# RHEL
 vendor_dir = os.path.join('/', 'usr', 'lib64', 'loudml', 'vendor')
+sys.path.insert(0, vendor_dir)
+
+# Debian
+vendor_dir = os.path.join('/', 'usr', 'lib', 'loudml', 'vendor')
 sys.path.insert(0, vendor_dir)
