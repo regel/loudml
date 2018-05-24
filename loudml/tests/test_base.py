8287e5b7024c8a12fb21b98b72d1b19abfdb6138
Sebastien Leger <sebastien.regel@gmail.com>
2018-05-24 11:17:19 +0200

    Add unit test for loudml-base

---
+++ loudml/tests/test_base.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import unittest
+import logging
+logging.getLogger('tensorflow').disabled = True
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        # RHEL
+        vendor_dir = os.path.join('/', 'usr', 'lib64', 'loudml', 'vendor')
+        sys.path.insert(0, vendor_dir)
+        
+        # Debian
+        vendor_dir = os.path.join('/', 'usr', 'lib', 'loudml', 'vendor')
+        sys.path.insert(0, vendor_dir)
+
+
+    def test_numpy(self):
+        import numpy as np
+        self.assertEqual(np.version.version, "1.14.3")
+
+    def test_scipy(self):
+        import scipy
+        self.assertEqual(scipy.__version__, "1.1.0")
+
+    def test_tf(self):
+        import tensorflow as tf
+        self.assertEqual(tf.__version__, "1.3.0")
+
+        # Simple hello world using TensorFlow
+        
+        # Create a Constant op
+        # The op is added as a node to the default graph.
+        #
+        # The value returned by the constructor represents the output
+        # of the Constant op.
+        hello = tf.constant('Hello, TensorFlow!')
+        
+        # Start tf session
+        sess = tf.Session()
+        
+        # Run the op
+        self.assertEqual(sess.run(hello), b'Hello, TensorFlow!')
+
