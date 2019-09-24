c5bf12d61f6fe8fab9317348f851fa2bf6b94000
Sebastien Leger <sebastien.regel@gmail.com>
2019-09-24 10:59:59 +0200

    fix version in setup.py

--- loudml/setup.py
+++ loudml/setup.py
@@ -5,12 +5,22 @@ Setup Loud ML python package
 """
 
 import os
+from io import open as io_open
 from setuptools import setup
+
+# Get version from loudml/_version.py
+__version__ = None
+src_dir = os.path.abspath(os.path.dirname(__file__))
+if 'LOUDML_VERSION' in os.environ:
+    __version__ = os.environ['LOUDML_VERSION']
+else:
+    version_file = os.path.join(src_dir, 'loudml', '_version.py')
+    with io_open(version_file, mode='r') as fd:
+        exec(fd.read())
 
 setup(
     name='loudml',
-
-    version=os.getenv('LOUDML_VERSION', '1.4'),
+    version=__version__,
 
     description="Machine Learning application",
 
