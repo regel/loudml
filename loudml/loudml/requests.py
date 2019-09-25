bf997ea6595643448e42b847227d7d0fb29f9e31
Sebastien Leger <sebastien.regel@gmail.com>
2019-09-25 09:21:24 +0200

    Use requests.Request to prepare scheduled_jobs requests
    The requests.py file and related exceptions classes are derived
    from Apache 2.0 elasticsearch-py and therefore cited in LICENSE file.

---
+++ loudml/loudml/requests.py
@@ -0,0 +1,38 @@
+import loudml.vendor  # noqa
+from loudml import errors
+import requests
+from urllib.parse import urlencode
+
+
+def perform_request(
+    base_url,
+    method,
+    url,
+    session,
+    params=None,
+    body=None,
+    timeout=None,
+    ignore=(),
+    headers=None
+):
+    url = base_url + url
+    if params:
+        url = '%s?%s' % (url, urlencode(params or {}))
+
+    request = requests.Request(
+        method=method, headers=headers, url=url, json=body)
+    prepared_request = session.prepare_request(request)
+    settings = session.merge_environment_settings(
+        prepared_request.url, {}, None, None, None)
+    send_kwargs = {'timeout': timeout}
+    send_kwargs.update(settings)
+    try:
+        response = session.send(prepared_request, **send_kwargs)
+    except Exception as e:
+        if isinstance(e, requests.exceptions.SSLError):
+            raise errors.SSLError('N/A', str(e), e)
+        if isinstance(e, requests.Timeout):
+            raise errors.ConnectionTimeout('TIMEOUT', str(e), e)
+        raise errors.ConnectionError('N/A', str(e), e)
+
+    return response
